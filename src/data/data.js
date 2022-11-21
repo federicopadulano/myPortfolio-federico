66979a929e717600a6ffb7c1a2060d380e3ea34c
federicopadulano <federicopadulanoplay@gmail.com>
2022-11-21 14:11:42 +0100

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+import Jobify from "../assets/projects/jobify.png";
+import Cocktail from "../assets/projects/cocktailProj.png";
+import Quiz from "../assets/projects/quiz.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "MERN Application",
+    image: Jobify,
+    github:
+      "https://github.com/federicopadulano/mern-jobify-federico-production",
+    live: "https://mern-jobify-federico.onrender.com/",
+  },
+  {
+    id: 2,
+    name: "ANGULAR Application",
+    image: Cocktail,
+    github: "",
+    live: "https://fedcocktail.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "REACT Application",
+    image: Quiz,
+    github: "https://github.com/federicopadulano/Quiz-Fed-App",
+    live: "https://fedquiz.netlify.app/",
+  },
+];
